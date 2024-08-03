f150eac7619e757b066b75d1d84a249cb22763b1
Sandro Baccega <sandro.baccega@wirecube.at>
2024-08-03 10:38:20 +0200

    🧑‍💻 Add credits in footer

--- src/app/[locale]/_components/Footer.tsx
+++ src/app/[locale]/_components/Footer.tsx
@@ -9,8 +9,8 @@ export default async function Footer(props: { locale: string }) {
   const year = new Date().getFullYear();
   return (
     <footer className="bg-primary-80">
-      <div className="container flex flex-col items-center justify-between gap-2 py-1 text-sm text-white md:flex-row md:px-16">
-        <nav className="flex flex-col gap-x-2 md:-translate-x-4 md:flex-row">
+      <div className="container flex flex-col items-center justify-between py-1 text-sm text-white md:flex-row md:px-16">
+        <nav className="flex flex-col flex-wrap gap-x-2 md:-translate-x-4 md:flex-row">
           <Link
             href={`/${props.locale}/privacy`}
             className={buttonVariants({
@@ -51,7 +51,29 @@ export default async function Footer(props: { locale: string }) {
             {t("returns")}
           </Link>
         </nav>
-        <p className="">{t("copyright", { year })}</p>
+        <div className="flex flex-col flex-wrap items-center justify-end gap-x-2 md:flex-row">
+          <p className="">
+            {t.rich("credits", {
+              sandro: (chunks) => (
+                <Link
+                  href={`https://github.com/Baccega`}
+                  className={buttonVariants({
+                    variant: "link",
+                    inverted: true,
+                    size: "sm",
+                  })}
+                >
+                  {chunks}
+                </Link>
+              ),
+            })}
+          </p>
+          <p className="flex h-9 items-center px-2">
+            {t("copyright", {
+              year,
+            })}
+          </p>
+        </div>
       </div>
     </footer>
   );
