0be0e75fc247cb011ed85b94ff629754b94f0fd2
Sandro Baccega <mail.sandro.baccega@gmail.com>
2025-03-07 14:31:06 +0100

    ✨ Sort products based on enabled value

--- src/server/stripeQueries.ts
+++ src/server/stripeQueries.ts
@@ -5,7 +5,12 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 export async function getStripeProducts() {
   const data = (await stripe.products.list({ expand: ["data.default_price"] }))
     .data;
-  return (JSON.parse(JSON.stringify(data)) as Stripe.Product[]);
+  const products = JSON.parse(JSON.stringify(data)) as Stripe.Product[];
+  return products.sort((a, b) => {
+    const enabledA = a.metadata?.enabled === "true" ? 1 : 0;
+    const enabledB = b.metadata?.enabled === "true" ? 1 : 0;
+    return enabledB - enabledA;
+  });
 }
 
 export async function getStripeProduct(id: string) {
