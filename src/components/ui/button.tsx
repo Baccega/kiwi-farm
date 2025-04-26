9702ff825d25c50c724e5688ad176df05a956601
Sandro Baccega <sandro.baccega@chax.at>
2025-04-26 15:04:33 +0200

    🎨 Use transition all on button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "~/lib/utils";
 
 const buttonVariants = cva(
-  "transition-all hover:scale-105 hover:shadow-lg active:scale-95 active:shadow-inner relative inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50",
+  "transition-all hover:scale-105 hover:shadow-lg active:scale-95 active:shadow-inner relative inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50",
   {
     variants: {
       variant: {
